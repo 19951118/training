53a72d5aed2ada05ef71944c1bd39145d954645d
19951118 <reonn1118@yahoo.co.jp>
2023-10-08 08:58:56 +0000

    16章まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :post_images, dependent: :destroy #アソシエーション、依存、削除
+  #アソシエーション、依存、削除
+  has_many :post_images, dependent: :destroy
+  #１対１の関係で擬似カラム作成（ActiveStorage）
+  has_one_attached :profile_image
+  def get_profile_image(width,height)
+    #ない限り　画像の添付が
+    unless profile_image.attached?
+      #file_pathにこのrootを教え加える
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      #画像添付（この引数の）このファイルをこの形式で開く。
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    #画像を変異させる（サイズを変える[横、縦]）これで処理する。
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  #ルートパス
-  root 'homes#top'
-  #ネーミング
-  get 'homes/about', as: "about"
-  #ルート一括作成、限定
-  resources :post_images, only: [:new, :create, :destroy, :show, :index]
+  root to: "homes#top"
+  get "homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
